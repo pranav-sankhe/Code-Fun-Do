0ac3e7f49bfc4ac80317ff1afb6f364d45ae534f
Pranav Sankhe <pranavsankhe40@gmail.com>
2017-04-11 22:02:03 +0530

    commit

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ bot.dialog('/', intents);
 intents.onDefault([
     function (session, args, next) {
         if (!session.userData.name) {
-            builder.Prompts.text(session, 'Can I collect some basic info from u ? You are not registered to me');
+            builder.Prompts.text(session, 'Hey!! Can I collect some basic info from u ? You are not registered to me');
         }
         else{
             session.beginDialog('/normal_chatting');
         }
     },
     function (session, results) {
-        if (results.response == 'Yes'){
+        if (results.response == 'Yes' || results.response == 'yes'){
             session.beginDialog('/get_profile');
         }
         else{
@@ -31,7 +31,7 @@ bot.dialog('/get_profile', [
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.text(session, 'Hi'+results.response+'your age ?');
+        builder.Prompts.number(session, 'Hi'+results.response+'your age ?');
     },
     function (session, results) {
         session.userData.age = results.response;
@@ -50,7 +50,7 @@ bot.dialog('/get_profile', [
         session.send("Got it... " + session.userData.name + 
                      " you've been programming for " + session.userData.coding + 
                      " years and use " + session.userData.language + ".");
-        session.beginDialog('/normal_chatting');
+        //session.beginDialog('/normal_chatting');
     },
     function (session, results){
         session.send('How\'s ur health' + session.userData.name + ' ?');
@@ -114,17 +114,18 @@ bot.dialog('/VeryBad',[
 
 bot.dialog('/normal_chatting', [
     function (session) {
-        builder.Prompts.text(session, 'Hi! What is your name?');
+        session.send('listen mode');
+        builder.Prompts.text(session, '');
     },
     function (session, results) {
-        session.send('Nice name');
-        session.userData.name = results.response;
-        session.endDialog();
+        if (results.response != ''){
+            session.beginDialog('/googling');
+        }   
     }
 ]);
 
 bot.dialog('/googling', [
-    function (session) {
+    function (session,results) {
         builder.Prompts.text(session , 'Do u want me to google that for u ?');
     },
     function (session, results) {
@@ -133,9 +134,120 @@ bot.dialog('/googling', [
         }
         else{
             session.send('Sorry then I cant help');
+            session.endDialog();
         }
     }
 ]);
+
+//------------------------------for the alarm part-------------------------------------------------//
+var model = 'https://api.projectoxford.ai/luis/v2.0/apps/c413b2ef-382c-45bd-8ff0-f76d60e2a821?subscription-key=62383e07722f440f9ffd28c4cb7ba535&q=';
+var recognizer = new builder.LuisRecognizer(model);
+var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', dialog);
+
+dialog.matches('builtin.intent.alarm.set_alarm', [
+    function (session, args, next) {
+        // Resolve and store any entities passed from LUIS.
+        var title = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
+        var time = builder.EntityRecognizer.resolveTime(args.entities);
+        var alarm = session.dialogData.alarm = {
+          title: title ? title.entity : null,
+          timestamp: time ? time.getTime() : null  
+        };
+        
+        // Prompt for title
+        if (!alarm.title) {
+            builder.Prompts.text(session, 'What would you like to call your alarm?');
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        var alarm = session.dialogData.alarm;
+        if (results.response) {
+            alarm.title = results.response;
+        }
+
+        // Prompt for time (title will be blank if the user said cancel)
+        if (alarm.title && !alarm.timestamp) {
+            builder.Prompts.time(session, 'What time would you like to set the alarm for?');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        var alarm = session.dialogData.alarm;
+        if (results.response) {
+            var time = builder.EntityRecognizer.resolveTime([results.response]);
+            alarm.timestamp = time ? time.getTime() : null;
+        }
+        
+        // Set the alarm (if title or timestamp is blank the user said cancel)
+        if (alarm.title && alarm.timestamp) {
+            // Save address of who to notify and write to scheduler.
+            alarm.address = session.message.address;
+            alarms[alarm.title] = alarm;
+            
+            // Send confirmation to user
+            var date = new Date(alarm.timestamp);
+            var isAM = date.getHours() < 12;
+            session.send('Creating alarm named "%s" for %d/%d/%d %d:%02d%s',
+                alarm.title,
+                date.getMonth() + 1, date.getDate(), date.getFullYear(),
+                isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
+        } else {
+            session.send('Ok... no problem.');
+        }
+    }
+]);
+
+
+dialog.matches('builtin.intent.alarm.delete_alarm', [
+    function (session, args, next) {
+        // Resolve entities passed from LUIS.
+        var title;
+        var entity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
+        if (entity) {
+            // Verify its in our set of alarms.
+            title = builder.EntityRecognizer.findBestMatch(alarms, entity.entity);
+        }
+        
+        // Prompt for alarm name
+        if (!title) {
+            builder.Prompts.choice(session, 'Which alarm would you like to delete?', alarms);
+        } else {
+            next({ response: title });
+        }
+    },
+    function (session, results) {
+        // If response is null the user canceled the task
+        if (results.response) {
+            delete alarms[results.response.entity];
+            session.send("Deleted the '%s' alarm.", results.response.entity);
+        } else {
+            session.send('Ok... no problem.');
+        }
+    }
+]);
+
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
+
+// Very simple alarm scheduler
+var alarms = {};
+setInterval(function () {
+    var now = new Date().getTime();
+    for (var key in alarms) {
+        var alarm = alarms[key];
+        if (now >= alarm.timestamp) {
+            var msg = new builder.Message()
+                .address(alarm.address)
+                .text("Here's your '%s' alarm.", alarm.title);
+            bot.send(msg);
+            delete alarms[key];
+        }
+    }
+}, 15000);
+//------------------------------for the alarm part-------------------------------------------------//
 /*
 var imported = document.createElement('script');
 imported.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js';
@@ -170,5 +282,5 @@ bot.dialog('/bingsearch',[
             });
     })
 ]);
-*/
 
+*/
